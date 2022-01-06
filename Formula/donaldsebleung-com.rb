e8ae59328cd24378eb331b3eddef2413f1454880
Donald Sebastian Leung <donaldsebleung@gmail.com>
2022-01-06 20:44:18 +0800

    Update donaldsebleung-com formula to v0.2.1

--- Formula/donaldsebleung-com.rb
+++ Formula/donaldsebleung-com.rb
@@ -1,8 +1,8 @@
 class DonaldsebleungCom < Formula
   desc "My personal website reinstated, this time written in Spring"
   homepage "https://github.com/DonaldKellett/donaldsebleung-com"
-  url "https://github.com/DonaldKellett/donaldsebleung-com/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "d608532929ea9865b84ae705b5ea8041a39d01caa9256858dd49fdae21091c76"
+  url "https://github.com/DonaldKellett/donaldsebleung-com/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "9e8aeb9cfd3a8414c1be8849c5f476d1f5d5ea9ccdb470db61f611473bffbae5"
   license "MIT"
   depends_on "maven" => :build
   depends_on "openjdk"
@@ -74,7 +74,7 @@ class DonaldsebleungCom < Formula
       
       case $1 in
         get-version)
-          echo "0.2.0"
+          echo "0.2.1"
           exit
           ;;
         install-key)
